883be262246da7e5def1965eee9917ff5717dab7
Darren White <dwhite@gentingcasinos.co.uk>
2020-08-10 19:10:49 +0100

    [ADD] stlyed components back in and tutorial code

--- src/hooks/use-user-media.js
+++ src/hooks/use-user-media.js
@@ -1,29 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useUserMedia = (requestedMedia) => {
+export function useUserMedia(requestedMedia) {
   const [mediaStream, setMediaStream] = useState(null);
 
   useEffect(() => {
-    const enableStream = async () => {
+    async function enableVideoStream() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia(
           requestedMedia
         );
-        console.log('enableStream -> stream', stream);
         setMediaStream(stream);
-      } catch (error) {
-        console.error('enableStream -> error', error)
+      } catch (err) {
+        // Handle the error
       }
     }
 
     if (!mediaStream) {
-      enableStream();
+      enableVideoStream();
     } else {
       return function cleanup() {
         mediaStream.getTracks().forEach(track => {
           track.stop();
-        })
-      }
+        });
+      };
     }
   }, [mediaStream, requestedMedia]);
 
--- src/index.js
+++ src/index.js
@@ -1,47 +1,43 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
-
-// Our Components
 import { Camera } from "./camera";
+import { Root, Preview, Footer, GlobalStyle } from "./styles";
 
 function App() {
   const [isCameraOpen, setIsCameraOpen] = useState(false);
   const [cardImage, setCardImage] = useState();
 
   return (
-    <div className="flex overflow-x-hidden overflow-y-scroll p-20">
-      {isCameraOpen && (
-        <Camera
-          onCapture={blob => setCardImage(blob)}
-          onClear={() => setCardImage(undefined)}
-        />
-      )}
+    <Fragment>
+      <Root>
+        {isCameraOpen && (
+          <Camera
+            onCapture={blob => setCardImage(blob)}
+            onClear={() => setCardImage(undefined)}
+          />
+        )}
 
-      {cardImage && (
-        <div>
-          <h2>Preview</h2>
-          <img src={cardImage && URL.createObjectURL(cardImage)} alt="Webcam preview" />
-        </div>
-      )}
+        {cardImage && (
+          <div>
+            <h2>Preview</h2>
+            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
+          </div>
+        )}
 
-      <footer className="fixed inset-x-0 bottom-0 p-6 flex justify-center align-center bg-gray-200">
-        <button
-          className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setIsCameraOpen(true)}
-        >
-          Open Camera
-        </button>
-        <button
-          className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            setIsCameraOpen(false);
-            setCardImage(undefined);
-          }}
-        >
-          Close Camera
-        </button>
-      </footer>
-    </div>
+        <Footer>
+          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
+          <button
+            onClick={() => {
+              setIsCameraOpen(false);
+              setCardImage(undefined);
+            }}
+          >
+            Close Camera
+          </button>
+        </Footer>
+      </Root>
+      <GlobalStyle />
+    </Fragment>
   );
 }
 
